9b55b26d42c36a44acc6e97d4288c6eb88200dec
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-05-07 17:45:47 +0530

    commit on 'master'

--- SLAPS/time.js
+++ SLAPS/time.js
@@ -2,11 +2,8 @@ var select = document.getElementById("timeManager");
 console.log(select)
 select.addEventListener('submit',function(e){
     e.preventDefault
-    var eros = select.querySelector('input[type="text"]').value
+    var eros = select.querySelector('input[type="time"]').value
     console.log(eros)
     alert("Your "+ eros + " hour time starts now!!")
     localStorage.setItem('time',eros);
 })
-
-
-
