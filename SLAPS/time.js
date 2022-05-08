25b1e7ae344aba7cfa84ccd71725699b8ac0e1a1
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-05-08 08:41:25 +0530

    commit on 'master'

--- SLAPS/time.js
+++ SLAPS/time.js
@@ -6,8 +6,9 @@ btn.addEventListener('click',function(e){
     var email = document.getElementById('email').value;
     console.log(name );
     var subject = "TIME LIMIT REACHED";
-    var message = "DEAR USER YOU HAVE CROSSED YOUR SET DATA HENCE WE REQUEST YOU TO END YOUR METAVESR SESSION OR THE WEBSITE THAT YOU HAVE OPENED TO KNOW MORE VISIT OUR WEBSITE.";
-    var body = 'name:' +name + '<br/> email:' + email +'<br/> subject' + subject + '<br/> message' + message;
+    var message = "DEAR USER YOU HAVE REACHED YOUR SET TIME LIMIT HENCE WE REQUEST YOU TO END YOUR METAVESR SESSION OR THE WEBSITE THAT YOU HAVE OPENED. TO KNOW MORE VISIT OUR WEBSITE.";
+    var body =message;
+    console.log(body)
     Email.send({
         Host : "smtp.yourisp.com",
         Username : "shobhitsingh25032003@gmail.com",
@@ -19,8 +20,9 @@ btn.addEventListener('click',function(e){
     }).then(
       message => alert("YOUR TIME STARTS NOW !")
     );
-
 })
+
+
 
 
 
